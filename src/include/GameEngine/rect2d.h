70e159b06903ab0bc2056abfb02fbc1ad8dc8807
= <=>
2025-04-20 13:14:10 -0400

    github copilot is tweakin so i just made my own vector classes

--- src/include/GameEngine/rect2d.h
+++ src/include/GameEngine/rect2d.h
@@ -2,72 +2,39 @@
 
 struct rect2d_t
 {
-    float x;
-    float y;
-    float w;
-    float h;
+    Vec2 pos;
+    Vec2 whv;
 
-    // this structure is used to calcute the bounding box of a sprite
+    // this structure is used to calcute the bounding bopos.x of a sprite
     // when a rect is drawn it is drawn from the center of the rect
-    Vec2 getCenter() const
-    {
-        return Vec2({x + w / 2, y + h / 2});
-    }
 
-    Vec2 getSize() const
-    {
-        return Vec2({w, h});
+    rect2d_t(Vec2 pos, Vec2 whv) : pos(pos), whv(whv) {
+        //
     }
-
-    Vec2 getTopLeft() const
-    {
-        return Vec2({x - w / 2, y - h / 2});
-    }
-    Vec2 getTopRight() const
-    {
-        return Vec2({x + w / 2, y - h / 2});
-    }
-
-    Vec2 getBottomLeft() const
-    {
-        return Vec2({x - w / 2, y + h / 2});
-    }
-
-    Vec2 getBottomRight() const
-    {
-        return Vec2({x + w / 2, y + h / 2});
-    }
-
-    Vec2 getTop() const
-    {
-        return Vec2({x, y - h / 2});
-    }
-
-    Vec2 getBottom() const
-    {
-        return Vec2({x, y + h / 2});
-    }
-
-    Vec2 getLeft() const
-    {
-        return Vec2({x - w / 2, y});
-    }
-
-    Vec2 getRight() const
-    {
-        return Vec2({x + w / 2, y});
-    }
-
-    Vec2 getSizeHalf() const
-    {
-        return Vec2({w / 2, h / 2});
-    }
-
-    Vec2 touching(const rect2d_t &other) const
-    {
-        return Vec2({x - other.x, y - other.y});
-    }
-
-    rect2d_t(float x, float y, float w, float h) : x(x), y(y), w(w), h(h) {}
-    rect2d_t() : x(0), y(0), w(0), h(0) {}
 };
+
+struct rectRectCollisonData {
+    rect2d_t a;
+    rect2d_t b;
+
+    bool touchingXY;
+    bool touchingX;
+    bool touchingY;
+    bool touchingNx; // negtive pos.x
+    bool touchingNy; // negtive pos.y
+
+    void update(){
+        // we get have beacuse when we display the rects we going to center them
+        Vec2 whA = a.getSizeHalf();
+        Vec2 whB = b.getSizeHalf();
+        Vec2 posA = a.pos;
+        Vec2 posB = b.pos;
+
+        touchingX = this.Aleft.x < this.Bright.x && this.Bleft.x < this.Aright.x
+        touchingY = this.Bdown.y > this.Aup.y && this.Adown.y > this.Bup.y
+        touchingXY = touchingX && touchingY
+        // these two are used to find wich quadrent the object falls into
+        touchingNx = this.b.pos.x > this.a.pos.x
+        touchingNy = this.a.pos.y > this.b.pos.y
+    }
+}
--- src/include/GameEngine/vectors.h
+++ src/include/GameEngine/vectors.h
@@ -5,88 +5,97 @@
 #include <string>
 #include <sstream>
 
-template <typename T, int N>
-class Vec {
-public:
-    T data[N];
+struct Vec2 {
+    float x;
+    float y;
 
-    Vec() {
-        for (int i = 0; i < N; ++i) data[i] = T();
+    Vec2(float x, float y) : x(x), y(y) {
+        //
     }
 
-    Vec(std::initializer_list<T> list) {
-        int i = 0;
-        for (auto& val : list) {
-            if (i < N) data[i++] = val;
-        }
+    Vec2 operator+(const Vec2& b) const { 
+        return Vec2(x + b.x, y + b.y);
     }
 
-    T& operator[](int index) { return data[index]; }
-    const T& operator[](int index) const { return data[index]; }
-
-    Vec operator+(const Vec& other) const {
-        Vec result;
-        for (int i = 0; i < N; ++i) result[i] = data[i] + other[i];
-        return result;
+    Vec2 operator-(const Vec2& b) const { 
+        return Vec2(x - b.x, y - b.y);
     }
 
-    Vec operator-(const Vec& other) const {
-        Vec result;
-        for (int i = 0; i < N; ++i) result[i] = data[i] - other[i];
-        return result;
+    Vec2 operator/(const Vec2& b) const { 
+        return Vec2(x / b.x, y / b.y);
     }
 
-    Vec operator*(const T& scalar) const {
-        Vec result;
-        for (int i = 0; i < N; ++i) result[i] = data[i] * scalar;
-        return result;
+    Vec2 operator*(const Vec2& b) const { 
+        return Vec2(x * b.x, y * b.y);
     }
 
-    Vec operator/(const T& scalar) const {
-        Vec result;
-        for (int i = 0; i < N; ++i) result[i] = data[i] / scalar;
-        return result;
+    Vec2 operator+=(const Vec2& b) const { 
+        return Vec2(x + b.x, y + b.y);
     }
 
-    Vec& operator+=(const Vec& other) {
-        for (int i = 0; i < N; ++i) data[i] += other[i];
-        return *this;
+    Vec2 operator-=(const Vec2& b) const { 
+        return Vec2(x - b.x, y - b.y);
     }
 
-    Vec& operator-=(const Vec& other) {
-        for (int i = 0; i < N; ++i) data[i] -= other[i];
-        return *this;
+    Vec2 operator/=(const Vec2& b) const { 
+        return Vec2(x / b.x, y / b.y);
     }
 
-    Vec& operator*=(const T& scalar) {
-        for (int i = 0; i < N; ++i) data[i] *= scalar;
-        return *this;
+    Vec2 operator*=(const Vec2& b) const { 
+        return Vec2(x * b.x, y * b.y);
     }
 
-    Vec& operator/=(const T& scalar) {
-        for (int i = 0; i < N; ++i) data[i] /= scalar;
-        return *this;
-    }
-
-    friend std::ostream& operator<<(std::ostream& os, const Vec& vec) {
-        os << "(";
-        for (int i = 0; i < N; ++i) {
-            os << vec[i];
-            if (i < N - 1) os << ", ";
-        }
-        os << ")";
+    friend std::ostream& operator<<(std::ostream& os, const Vec2& b) {
+        os << '(' << b.x << ',' << b.y << ')';
         return os;
-    }
-
-    friend std::string operator+(const std::string& str, const Vec& vec) {
-        std::ostringstream oss;
-        oss << str << vec;
-        return oss.str();
     }
 };
 
-using Vec2 = Vec<float, 2>;
-using Vec3 = Vec<float, 3>;
-using Vec4 = Vec<float, 4>;
+struct Vec3 {
+    float x;
+    float y;
+    float z;
+
+    Vec3(float x, float y, float z) : x(x), y(y), z(z) {
+        //
+    }
+
+    Vec3 operator+(const Vec3& b) const { 
+        return Vec3(x + b.x, y + b.y, z + b.z);
+    }
+
+    Vec3 operator-(const Vec3& b) const { 
+        return Vec3(x - b.x, y - b.y, z - b.z);
+    }
+
+    Vec3 operator/(const Vec3& b) const { 
+        return Vec3(x / b.x, y / b.y, z / b.z);
+    }
+
+    Vec3 operator*(const Vec3& b) const { 
+        return Vec3(x * b.x, y * b.y, z * b.z);
+    }
+
+    Vec3 operator+=(const Vec3& b) const { 
+        return Vec3(x + b.x, y + b.y, z + b.z);
+    }
+
+    Vec3 operator-=(const Vec3& b) const { 
+        return Vec3(x - b.x, y - b.y, z - b.z);
+    }
+
+    Vec3 operator/=(const Vec3& b) const { 
+        return Vec3(x / b.x, y / b.y, z / b.z);
+    }
+
+    Vec3 operator*=(const Vec3& b) const { 
+        return Vec3(x * b.x, y * b.y, z * b.z);
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Vec3& b) {
+        os << '(' << b.x << ',' << b.y << ',' << b.z << ')';
+        return os;
+    }
+};
 
 #endif // VECTORS_H
